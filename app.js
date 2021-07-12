b7dd8a13de17fe4192be1955e18302ace0f4d554
dgstjohn <dgstjohn@gmail.com>
2021-07-12 18:02:44 -0400

    added files, modularized code

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This....
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const fs = require('fs');
 
-    console.log('============');
+const generatePage = require('./src/page-template.js');
 
-    // ....is the same as this
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// old 9.1 exercise
+// const printProfileData = profileDataArr => {
+//     // This....
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('============');
+
+//     // ....is the same as this
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+
+// printProfileData(profileDataArgs);
